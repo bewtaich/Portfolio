33e30006af222edb1b97a0f7483715bd9801c58a
Bewtaich <bewtaich@gmail.com>
2024-09-30 17:32:47 -0500

    updated file structure

---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <section>
+            <img src="" alt="Brian Taich"/>
+            <p>My Bio</p>
+        </section>
+    )
+}
+
+export default AboutMe;
